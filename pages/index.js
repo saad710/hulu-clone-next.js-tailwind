175d98fa4da2f13bfcd0c41d5b0332f80a0dd79f
saad710 <ifrathasnat2@gmail.com>
2021-08-02 19:42:14 +0600

    hulu-clone-done

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,12 @@ import Head from 'next/head'
 
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests from '../utlis/requests'
 
 
-export default function Home() {
+export default function Home({results}) {
+  console.log({results})
   return (
     <div>
       <Head>
@@ -16,6 +19,24 @@ export default function Home() {
     
       <Header/>
       <Nav/>
+      <Results results ={results}/>
+     
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
