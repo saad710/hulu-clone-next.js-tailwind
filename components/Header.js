7085b1d97f028110c5427b9105df013f91a0468f
saad710 <ifrathasnat2@gmail.com>
2021-08-02 13:28:30 +0600

    nav-issues-solved

--- components/Header.js
+++ components/Header.js
@@ -12,8 +12,15 @@ import {
 
 function Header() {
     return (
-        <header>
-            <HeaderItem title='HOME' Icon ={HomeIcon}/>
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+          <div className="flex flex-grow justify-evenly max-w-2xl">
+          <HeaderItem title='HOME' Icon ={HomeIcon}/>
+            <HeaderItem title='COLLECTION' Icon ={CollectionIcon}/>
+            <HeaderItem title='VERIFIED' Icon ={BadgeCheckIcon}/>
+            <HeaderItem title='TRENDING' Icon ={LightningBoltIcon}/>
+            <HeaderItem title='SEARCH' Icon ={SearchIcon}/>
+            <HeaderItem title='ACCOUNT' Icon ={UserIcon}/>
+          </div>
            <Image 
            alt =''
            className = "object-contain"
